540329ee6ebac1eb26580a926c673079ed18efb6
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-08-25 16:12:19 +0200

    Modified package.json and other files to deploy backend

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -3,17 +3,18 @@ import { DataSource } from "typeorm";
 import { User } from "../entities/userEntitie";
 import { Credential } from "../entities/credentialEntitie";
 import { Appointment } from "../entities/appointmentEntitie";
-import { DB_HOST, DB_PORT, DB_USERNAME, DB_NAME, DB_PASSWORD } from "./envs";
+// import { DB_HOST, DB_PORT, DB_USERNAME, DB_NAME, DB_PASSWORD } from "./envs";
 
 // dropSchema: true, // Uncomment this line to drop the schema on every connection, this setting delete all the data in the database, use it only for development purposes.
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: DB_HOST,
-  port: parseInt(DB_PORT || "5432"),
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  url: process.env.DATABASE_URL,
+  // host: DB_HOST,
+  // port: parseInt(DB_PORT || "5432"),
+  // username: DB_USERNAME,
+  // password: DB_PASSWORD,
+  // database: DB_NAME,
   synchronize: true,
   // dropSchema: true,
   logging: ["error"],
